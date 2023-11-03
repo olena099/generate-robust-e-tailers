0dffe80d215aa0a5350d14d6f54e6e7394ca4648
olena099 <142909583+olena099@users.noreply.github.com>
2023-11-02 22:29:44 -0700

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,16 @@
+function main() {
+main();
+
+    const data = [];
+    const data = generateRandomData();
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    }
+    });
+    return data;
+        console.log("Random Number: " + item);
+}
